f6308dd75ee7fac183c136b5b842c2955b2fdef1
aradwian29 <dwiandinitiara965@gmail.com>
2026-04-20 13:25:31 +0700

    Update fitur hapus catatan

--- Main.java
+++ Main.java
@@ -5,11 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<String> catatan = new ArrayList<>();
+        catatan.add("Belajar java");
+        catatan.add("Ngoding CLI");
+        catatan.add("Kerjain Tugas");
 
         System.out.println("=== CATATAN HARIAN ===");
         System.out.println("1. Tambah Catatan");
         System.out.println("2. Lihat Catatan");
-        System.out.println("3. Keluar");
+        System.out.println("3. Hapus Catatan");
+        System.out.println("4. Keluar");
+
 
         System.out.print("Pilih menu: ");
         int pilihan = input.nextInt();
@@ -30,13 +35,27 @@ public class Main {
                 }
             }
         } else if (pilihan == 3) {
+            if (catatan.isEmpty()) {
+                System.out.println("Belum ada catatan yang bisa dihapus");
+            } else {
+                System.out.println("Daftar catatan");
+                for (int i = 0; i < catatan.size(); i++) {
+                    System.out.println((i+1) + "." + catatan.get(i));
+                }
+                System.out.println("Pilih nomor catatan yang mau dihapus: ");
+                int nomorHapus = input.nextInt();
+                if (nomorHapus >= 1 && nomorHapus <= catatan.size()) {
+                    catatan.remove(nomorHapus - 1);
+                    System.out.println("Catatan berhasil dihapus.");
+                } else {
+                    System.out.println("Nomor catatan tidak valid.");
+                }
+            }
+        } else if (pilihan == 4) {
             System.out.println("Program selesai");
         } else {
             System.out.println("Pilihan tidak valid");
         }
-
-        System.out.println("Kamu memilih menu: " + pilihan);
     input.close();
     }
-    
 }
